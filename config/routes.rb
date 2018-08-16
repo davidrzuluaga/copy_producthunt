ad01de3d78f06ec749bc3f807302f3a84725df80
davidrzuluaga <git@davidrzuluaga.com>
2018-08-16 17:22:09 -0500

    se agrego el formulario

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'product/index'
   get 'products', to: 'product#index'
+  get 'products/new', to: 'product#new' 
+  post '/products', to: 'product#create'
 end
