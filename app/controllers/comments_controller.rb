f4b2f11805cf0b16801ab25cff41ddf5721ae485
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-21 10:48:17 -0500

    implementa comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,15 @@ class CommentsController < ApplicationController
         redirect_to product
     end
 
+    def destroy
+        @comment = Product.find(params[:product_id])
+        if @comment.user == current_user
+          comment = Product.find(params[:product_id])
+          comment.destroy
+          redirect_to products_path
+        end    
+    end
+
   private
     def comments_params
         params.require(:comment).permit(:body).merge(user: current_user)
