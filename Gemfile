b8f1d17a596053680e630a632b3568e325190960
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-21 09:46:53 -0500

    Implementa la funcionalidad de comentarios

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
 
+gem 'pry'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/', to: 'product#index'
-  get 'products', to: 'product#index'
-  get 'products/new', to: 'product#new', as: 'new_product'
-  post '/products', to: 'product#create'
-  get 'products/:id', to: 'product#show', as: 'product'
-  get '/products/:id/edit', to: 'product#edit'
-  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  delete '/products/:id', to: 'product#destroy'
-  patch '/products/:id', to: 'product#update'  
+  root 'product#index'
+
+  resources :product do
+    resources :comments, only: [:create]
+  end
 end
+
+##resources :comments # es lo mismo que poner todo esto:
+# get 'products', to: 'product#index'
+# get 'products/new', to: 'product#new', as: 'new_product'
+# post '/products', to: 'product#create'
+# get 'products/:id', to: 'product#show', as: 'product'
+# get '/products/:id/edit', to: 'product#edit'
+# get '/products/:id/edit', to: 'product#edit', as: 'edit_product'
+# delete '/products/:id', to: 'product#destroy'
+# patch '/products/:id', to: 'product#update' 
